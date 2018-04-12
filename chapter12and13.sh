2eb52735e510e6d43f86c96becc931b8ff88d3cc
luschool <35786777+luschool@users.noreply.github.com>
2018-04-12 14:50:01 -0500

    final

---
+++ chapter12and13.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#Chapter 12 and 13 script by Lucas Merriott
+
+echo "Quick copy is an awesome feature if you want to move a file over your local network"
+echo "without worrying about folder sharing or anything else. It does this by temporarily"
+echo "opening a very simple http server on the host machine and allowing you to access"
+echo "it from other machines on the network."
+sleep 3s
+echo
+echo "The command you need to run is 'python -m SimpleHTTPServer' and then you can access"
+echo "the server from other machines by going to http://LOCALIP:8000"
+echo "The local IP portion will be your local machines ip for example 10.1.2.4"
+sleep 3s
+echo
+echo $''
+echo $'      _,.'
+echo $'    ,` -.)'
+echo $'   \'( _/\'-\\\\-.'
+echo $'  /,|`--._,-^|          ,'
+echo $'  \\_| |`-._/||          ,\'|'
+echo $'    |  `-, / |         /  /'
+echo $'    |     || |        /  /'
+echo $'     `r-._||/   __   /  /'
+echo $' __,-<_     )`-/  `./  /'
+echo $'\'  \\   `---\'   \\   /  /'
+echo $'    |           |./  /'
+echo $'    /           //  /'
+echo $'\\_/\' \\         |/  /'
+echo $' |    |   _,^-\'/  /'
+echo $' |    , ``  (\\/  /_'
+echo $'  \\,.->._    \\X-=/^'
+echo $'  (  /   `-._//^`'
+echo $'   `Y-.____(__}'
+echo $'    |     {__)'
+echo $'          ()`'
+echo "Most of chapter 12 and 13 covers configuring files and services that wouldn't"
+echo "be accessible or shown easily from a script thats run on another machine."
+echo "So here is a fearless warrior I copy pasted to protect my grade."
